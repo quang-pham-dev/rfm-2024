87e92a304d8270824e8e684b2089964dbd415690
quangpham-dev <quangpn.developer@gmail.com>
2024-08-07 12:05:59 +0700

    Add landing page

---
+++ apps/rfm-landing-page-app/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'rfm-landing-page-app',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/rfm-landing-page-app',
+};
---
+++ apps/rfm-landing-page-app/src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.tailwind file.
+   */
+  return (
+    <div id="welcome">
+      <h1>
+        <span>Hello there,</span>Welcome rfm-landing-page-app 👋
+      </h1>
+    </div>
+  );
+}
